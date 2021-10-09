c151decc9fec07f4839ff2ab659b23e88b21cc03
רפאל טולדנו <ratoleda@g.jct.ac.il>
2021-10-09 20:42:10 +0300

    Initial code + Welcome functions

---
+++ Targil0/Program5437.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome5437();
+            Console.ReadKey();
+        }
+
+        static partial void welcome1264();
+        private static void welcome5437()
+        {
+            string name;
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
