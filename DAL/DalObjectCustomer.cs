bd18b6c9402774da9e03e7c3f313ad166242903d
adadHa <adadi@yatmal.co.il>
2021-11-02 19:54:12 +0200

    seperate DalObject module

---
+++ DAL/DalObjectCustomer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DalObject
+{
+    public partial class DalObject
+    {
+        public DalObject()
+        {
+            DataSource.Initialize();
+        }
+
+        // This function add a customer to the customers data base.
+        public void AddCustomer(int id, string name, string phoneNumber, double longitude, double latitude)
+        {
+            DataSource.Customers.Add( new IDAL.DO.Customer()
+            {
+                Id = id,
+                Name = name,
+                Phone = phoneNumber,
+                Longitude = longitude,
+                Latitude = latitude
+            });
+        }
+
+        //This function returns a copy of the customers list.
+        public IEnumerable<IDAL.DO.Customer> ViewCustomersList()
+        {
+            List<IDAL.DO.Customer> resultList = new List<IDAL.DO.Customer>();
+            foreach (IDAL.DO.Customer customer in DataSource.Customers)
+            {
+                IDAL.DO.Customer c = new IDAL.DO.Customer();
+                c = customer;
+                resultList.Add(c);
+            }
+            return resultList;
+        }
+
+        //This function returns the customer with the required Id.
+        public IDAL.DO.Customer ViewCustomer(int id)
+        {
+            int index = DataSource.Customers.FindIndex(x => x.Id == id);
+            return DataSource.Customers[index];
+        }
+    }
+}
