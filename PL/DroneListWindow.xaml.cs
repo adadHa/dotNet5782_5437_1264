4511af830dab8b08728811a332a5ba1242428e5c
eadadi <adadi@yatmal.co.il>
2021-12-01 16:12:17 +0200

    PL cancel selections option on combobox

--- PL/DroneListWindow.xaml.cs
+++ PL/DroneListWindow.xaml.cs
@@ -32,6 +32,7 @@ namespace PL
             BLObject = blObject;
             DronesListView.ItemsSource = BLObject.GetDrones();
             StatusSelector.ItemsSource = Enum.GetValues(typeof(DroneStatuses));
+            WeightSelector.ItemsSource = Enum.GetValues(typeof(WheightCategories));
         }
 
         private void StatusSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -39,5 +40,19 @@ namespace PL
             DroneStatuses statusFilter = (DroneStatuses)StatusSelector.SelectedItem;
             DronesListView.ItemsSource = BLObject.GetDrones(x => x.Status == statusFilter);
         }
+        private void WeightSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            WheightCategories weightFilter = (WheightCategories)WeightSelector.SelectedItem;
+            DronesListView.ItemsSource = BLObject.GetDrones(x => x.MaxWeight == weightFilter);
+        }
+        private void StatusSelector_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            DronesListView.ItemsSource = BLObject.GetDrones();
+        }
+        private void WeightSelector_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            DronesListView.ItemsSource = BLObject.GetDrones();
+        }
+
     }
 }
