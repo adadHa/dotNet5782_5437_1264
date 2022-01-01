1196f298796a95c4ec13dd227ced1339baebfae9
רפאל טולדנו <ratoleda@g.jct.ac.il>
2022-01-01 21:14:26 +0200

    customer

--- PL/CustomerWindow.xaml.cs
+++ PL/CustomerWindow.xaml.cs
@@ -20,12 +20,80 @@ namespace PL
     public partial class CustomerWindow : Window
     {
         private IBL BLObject { get; set; }
+        private int Id;
+        private int Phone;
+        private double Longitude;
+        private double Longitude;
+
 
         // options/add mode
         public CustomerWindow(BO.Customer customer = null)
         {
             InitializeComponent();
             BLObject = BlFactory.GetBl();
+        }
+
+        private void ButtonAddCustomer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void IdValueTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if ((int.TryParse(IdValueTextBox.Text, out Id) && Id >= 0) || IdValueTextBox.Text == "")
+            {
+                IdValueTextBox.Background = Brushes.White;
+            }
+            else
+            {
+                IdValueTextBox.Background = Brushes.Red;
+            }
+        }
+
+        private void NameValueTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Name = NameValueTextBox.Text;
+        }
+
+        private void PTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void PhoneTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if ((int.TryParse(PhoneValueTextBox.Text, out Phone) && Phone >= 0) || PhoneValueTextBox.Text == "" || PhoneValueTextBox.Text.Length == 10)
+            {
+                PhoneValueTextBox.BorderBrush = Brushes.Green;
+            }
+            else
+            {
+                PhoneValueTextBox.BorderBrush = Brushes.Red;
+            }
+        }
+
+        private void LongitudeValueTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if ((double.TryParse(LongitudeValueTextBox.Text, out Longitude) && Longitude >= 0) && (double.TryParse(LongitudeValueTextBox.Text, out Longitude) && Longitude <= 180) || LongitudeValueTextBox.Text == "")
+            {
+                LongitudeValueTextBox.Background = Brushes.White;
+            }
+            else
+            {
+                LongitudeValueTextBox.Background = Brushes.Red;
+            }
+        }
+
+        private void LattitudeValueTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if ((double.TryParse(LongitudeValueTextBox.Text, out Longitude) && Longitude >= 0) && (double.TryParse(LongitudeValueTextBox.Text, out Longitude) && Longitude <= 180) || LongitudeValueTextBox.Text == "")
+            {
+                LongitudeValueTextBox.Background = Brushes.White;
+            }
+            else
+            {
+                LongitudeValueTextBox.Background = Brushes.Red;
+            }
         }
     }
 }
