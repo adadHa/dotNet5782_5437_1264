8f7d06c25564dce85010df66e1bdaeec26f0802d
adadHa <adadi@yatmal.co.il>
2022-01-02 17:44:12 +0200

    stations view

---
+++ PL/StationWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BO;
+using BlApi;
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for StationWindow.xaml
+    /// </summary>
+    public partial class StationWindow : Window
+    {
+        IBL BLObject = BlFactory.GetBl();
+        private int Id;
+        private string Name;
+        private int freeChargingSlots;
+        private double Longitude;
+        private double Lattitude;
+        public StationWindow(StationForList station = null)
+        {
+            InitializeComponent();
+            if(station == null) // add mode
+            {
+
+
+            }
+            else
+            {
+
+            }
+        }
+
+        private void ButtonAddCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(IdValueTextBox.Text, out Id) || Id < 0)
+            {
+                MessageBox.Show("Id should be an integer grater or equal to than 0!");
+            }
+
+            if ((double.TryParse(LongitudeValueTextBox.Text, out Longitude) && Longitude >= 0) && (double.TryParse(LongitudeValueTextBox.Text, out Longitude) && Longitude <= 180) || LongitudeValueTextBox.Text == "")
+            {
+                MessageBox.Show("Phone should be an double grater or equal to than 0 and need to be less or equal to 180!");
+            }
+
+            if ((double.TryParse(LattitudeValueTextBox.Text, out Lattitude) && Lattitude >= 0) && (double.TryParse(LattitudeValueTextBox.Text, out Longitude) && Lattitude <= 180) || LattitudeValueTextBox.Text == "")
+            {
+                MessageBox.Show("Phone should be an double grater or equal to than 0 and need to be less or equal to 180!");
+            }
+
+            else
+            {
+                try
+                {
+                    BLObject.AddStation(Id, Name,, new BO.Location { Longitude = this.Longitude, Latitude = this.Lattitude });
+                    MessageBox.Show($"Customer {Id} was added succefully!");
+                    Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+    }
+}
--- PL/StationsListPage.xaml.cs
+++ PL/StationsListPage.xaml.cs
@@ -12,7 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using BO;
+using BlApi;
 namespace PL
 {
     /// <summary>
@@ -20,9 +21,45 @@ namespace PL
     /// </summary>
     public partial class StationsListPage : Page
     {
+        IBL blObject = BlFactory.GetBl();
         public StationsListPage()
         {
             InitializeComponent();
+            StationsListView.DataContext = blObject.GetStations();
+        }
+
+        private void AddStationButton_Click(object sender, RoutedEventArgs e)
+        {
+            //DroneWindow droneWindow = new DroneWindow();
+            //droneWindow.Closed += DroneWindow_Closed;
+            //droneWindow.Show();
+        }
+        private void StationWindow_Closed(object sender, EventArgs e)
+        {
+            StationsListView.Items.Refresh();
+        }
+        private void StationsListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            StationForList drone = (StationForList)((ListView)sender).SelectedItem;
+            if (drone != null)
+            {
+               // new StationWindow(drone).Show();
+            }
+        }
+
+        private void orderByFreeSlots_Checked(object sender, RoutedEventArgs e)
+        {
+            var currenList = (IEnumerable<StationForList>)StationsListView.DataContext;
+            var resultList = from station in currenList
+                             orderby station.AvailableChargingSlots
+                             select station;
+            StationsListView.DataContext = resultList;
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            StationsListView.DataContext = blObject.GetStations();
+            orderByFreeSlots.IsChecked = false;
         }
     }
 }
